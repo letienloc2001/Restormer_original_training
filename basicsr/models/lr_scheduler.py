45b34af232d07f6f13877ab62add0e71cac350aa
letienloc <letienloc2001@gmail.com>
2022-12-03 14:44:15 +0700

    init commit

--- basicsr/models/lr_scheduler.py
+++ basicsr/models/lr_scheduler.py
@@ -220,10 +220,6 @@ class CosineAnnealingRestartCyclicLR(_LRScheduler):
     def get_lr(self):
         idx = get_position_from_periods(self.last_epoch,
                                         self.cumulative_period)
-        print('---------------------', self.last_epoch)
-        print('---------------------', self.cumulative_period)
-        print('---------------------', self.restart_weights)
-        print('---------------------', idx)
         current_weight = self.restart_weights[idx]
         nearest_restart = 0 if idx == 0 else self.cumulative_period[idx - 1]
         current_period = self.periods[idx]
